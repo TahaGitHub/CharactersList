e0095e385a297afa4fb24b5df06e2ec534279e75
taha.almokahel@gmail.com <taha.almokahel@gmail.com>
2022-07-16 21:43:54 +0300

    Implemented Search and Filter Components

--- src/components/FilterComponent.js
+++ src/components/FilterComponent.js
@@ -1,9 +1,80 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {StyleSheet, Text} from 'react-native';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {Checkbox} from 'react-native-paper';
 
-export default ({route}) => {
-  return <Text style={styles.container}>Search Component</Text>;
+export default ({dataSource, filteredDataSource, setFilteredDataSource}) => {
+  const [search, setSearch] = useState('');
+  const [checkedList, setcheckedList] = useState([1, 2, 3, 4, 5]);
+
+  let checkboxs = [1, 2, 3, 4, 5, 'All'];
+
+  const searchFilterFunction = (text, checkedbox) => {
+    let _checkedList = [];
+    if (checkedbox) {
+      if (checkedbox === 'All') {
+        _checkedList = [...checkboxs];
+      } else if (!checkedList.includes(checkedbox)) {
+        _checkedList = [...checkedList, checkedbox];
+      } else {
+        _checkedList = checkedList.filter(item => item !== checkedbox);
+      }
+      setcheckedList(_checkedList);
+    }
+    // console.log(text + ' ' + search + ' ' + checkedbox);
+    if (text || (checkedbox && search)) {
+      let _text = text || search;
+      let __checkedList = checkedbox ? _checkedList : checkedList;
+      // console.log(_text + ' ' + __checkedList);
+      const newData = dataSource.filter(item => {
+        const itemData = item.name ? item.name.toUpperCase() : ''.toUpperCase();
+        const textData = _text.toUpperCase();
+        return (
+          itemData.indexOf(textData) > -1 &&
+          item.appearance.some(v => __checkedList.includes(v))
+        );
+      });
+      setFilteredDataSource(newData);
+      setSearch(_text);
+    } else if (checkedbox) {
+      const newData = dataSource.filter(item =>
+        item.appearance.some(v => _checkedList.includes(v)),
+      );
+      setFilteredDataSource(newData);
+    } else {
+      setFilteredDataSource(dataSource);
+      setSearch(text);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.textInputStyle}
+        onChangeText={text => searchFilterFunction(text, null)}
+        value={search}
+        underlineColorAndroid="transparent"
+        placeholder="Search Here"
+      />
+
+      <Text>Filter By Appearance</Text>
+      <View style={styles.checkboxsStyle}>
+        {checkboxs.map((checkbox, index) => (
+          <View style={styles.checkboxStyle} key={index}>
+            <Text style={styles.checkboxStyle.text}>{checkbox}</Text>
+            <Checkbox
+              status={
+                checkedList.includes(checkbox) && checkbox !== 'All'
+                  ? 'checked'
+                  : 'unchecked'
+              }
+              onPress={() => searchFilterFunction(null, checkbox)}
+            />
+          </View>
+        ))}
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -11,5 +82,30 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     textAlign: 'center',
+  },
+  textInputStyle: {
+    width: '90%',
+    height: 60,
+    fontSize: 23,
+    borderColor: 'blue',
+    opacity: 0.5,
+    borderWidth: 2,
+    borderRadius: 10,
+    marginVertical: 10,
+    textAlign: 'center',
+  },
+
+  checkboxsStyle: {
+    width: '90%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  checkboxStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+
+    text: {
+      fontSize: 17,
+    },
   },
 });
--- src/screens/charactersList/CharactersList.js
+++ src/screens/charactersList/CharactersList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import {ScrollView, StyleSheet, useColorScheme, View} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
@@ -9,9 +9,13 @@ import FilterComponent from '../../components/FilterComponent';
 
 export default () => {
   const isDarkMode = useColorScheme() === 'dark';
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
 
   let {characters} = store.getState().characters;
   // console.log(characters);
+  useEffect(() => {
+    setFilteredDataSource(characters);
+  }, []);
 
   console.log('Rendiring Characher List Screen');
 
@@ -19,12 +23,17 @@ export default () => {
     <View
       style={{
         backgroundColor: isDarkMode ? Colors.black : Colors.white,
+        minHeight: '100%',
       }}>
-      <FilterComponent />
+      <FilterComponent
+        dataSource={characters}
+        filteredDataSource={filteredDataSource}
+        setFilteredDataSource={setFilteredDataSource}
+      />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <View style={styles.container}>
           {/* {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((character, index) => ( */}
-          {characters.map((character, index) => (
+          {filteredDataSource.map((character, index) => (
             <CharacterItem key={index} character={character} />
           ))}
         </View>
@@ -37,6 +46,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 17,
-    paddingVertical: 10,
+    paddingTop: 10,
+    paddingBottom: 100,
   },
 });
